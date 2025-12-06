d3e5dcc1aef78bedfd24ba2dd8f2b842036c5a3e
Brian Chew <brian@bchewy.com>
2025-12-06 19:36:03 +0800

    Chore:OG

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -11,6 +11,18 @@ export const renderer = jsxRenderer(({ children }) => {
         <meta name="description" content="Simple file storage powered by Cloudflare" />
         <meta name="theme-color" content="#f97316" />
         <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📦</text></svg>" />
+        
+        {/* Open Graph */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://flarebox.bchewy.com" />
+        <meta property="og:title" content="Flarebox" />
+        <meta property="og:description" content="Simple file storage powered by Cloudflare" />
+        <meta property="og:image" content="https://flarebox.bchewy.com/og-image.png" />
+        
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Flarebox" />
+        <meta name="twitter:description" content="Simple file storage powered by Cloudflare" />
         <ViteClient />
         <Link href="/src/style.css" rel="stylesheet" />
       </head>
